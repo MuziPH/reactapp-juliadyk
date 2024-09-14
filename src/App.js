3123eaef30f33bd20b6b32871c577e1099c1d775
Muzi PH <muzi.phage@gmail.com>
2024-09-14 08:43:40 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
       <h1>Welcome to my react app</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact Us</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/about' element={<About/>}/>
+            <Route exact path='/contact' element={<Contact />}/>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
